faf73c44627230b6260310c618299514ebe76730
Ayomide Basit <ayomidebasit026@gmail.com>
2025-03-28 04:25:11 -0700

    third commit

--- src/app/services/[serviceType]/page.jsx
+++ src/app/services/[serviceType]/page.jsx
@@ -3,35 +3,34 @@ import ServicesDetails from "@/components/services-details";
 import complete_services_data from "@/data/complete-services-data";
 import { usePathname } from "next/navigation";
 
-export const getStaticPaths = async () => {
-  const data = complete_services_data;
+// export const getStaticPaths = async () => {
+//   const data = complete_services_data;
 
-  const paths = data.map((service) => ({
-    params: { path: service.path.toString() },
-  }));
+//   const paths = data.map((service) => ({
+//     params: { path: service.path.toString() },
+//   }));
 
-  return { paths, fallback: false };
-};
+//   return { paths, fallback: false };
+// };
 
-export const getStaticProps = async (context) => {
-  const currentRoute = usePathname();
-  const path = context.params.path;
+// export const getStaticProps = async (context) => {
+//   const currentRoute = usePathname();
+//   const path = context.params.path;
 
-  const data = complete_services_data.map((service) => {
-    if (service.path === currentRoute) {
-      return service;
-    }
-    return;
-  });
+//   const data = complete_services_data.map((service) => {
+//     if (service.path === currentRoute) {
+//       return service;
+//     }
+//     return;
+//   });
 
-  return { props: { service: data } };
-};
+//   return { props: { service: data } };
+// };
 
-const page = ({ service }) => {
+const page = () => {
   return (
     <>
-      <ServicesDetails data={service} />
-      <h1>{service?.desc}</h1>
+      <ServicesDetails />
     </>
   );
 };
